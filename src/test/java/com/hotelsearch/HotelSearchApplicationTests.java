2de8da29adf39ba98cbca1fb482c7262939531ff
lucas.varone <lucas.varone@by.com.es>
2026-04-23 20:29:06 +0200

    Initial project structure

---
+++ src/test/java/com/hotelsearch/HotelSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
